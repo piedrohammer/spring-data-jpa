795b6bce672f1085d377c7a0444c28c802a80367
piedrohammer <piiedrohammer@gmail.com>
2023-02-23 14:00:32 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/service/IAlunoService.java
@@ -0,0 +1,22 @@
+package me.dio.academia.digital.service;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+
+import java.util.List;
+
+public interface IAlunoService {
+    abstract Aluno create(AlunoForm form);
+
+    Aluno get(Long id);
+
+    List<Aluno> getAll(String dataDeNascimento);
+
+    Aluno update(Long id, AlunoUpdateForm formUpdate);
+
+    void delete(Long id);
+
+    List<AvaliacaoFisica> getAllAvaliacaoFisicaId(Long id);
+}
